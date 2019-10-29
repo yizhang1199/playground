03782655c2d6b207f01090212390e58481637620
yzhang <yizhang1199@gmail.com>
2019-10-29 16:36:32 -0700

    Added samples

--- akka-quickstart-scala/iot-sample/src/main/scala/com/example/iot/DeviceGroup.scala
+++ akka-quickstart-scala/iot-sample/src/main/scala/com/example/iot/DeviceGroup.scala
@@ -2,59 +2,62 @@ package com.example.iot
 
 import akka.actor
 import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
-import akka.actor.SupervisorStrategy.Stop
-import com.example.iot.DeviceGroup.{ReplyDeviceList, RequestAllTemperatures, RequestDeviceList}
 import com.example.iot.DeviceManager.RequestTrackDevice
+
+import scala.collection.immutable
 
 object DeviceGroup {
   def props(groupId: String): Props = actor.Props(new DeviceGroup(groupId))
+
   final case class RequestDeviceList(requestId: Long)
+
   final case class ReplyDeviceList(requestId: Long, ids: Set[String])
 
   final case class RequestAllTemperatures(requestId: Long)
+
   final case class RespondAllTemperatures(requestId: Long, temperatures: Map[String, TemperatureReading])
 
   sealed trait TemperatureReading
+
   final case class Temperature(value: Double) extends TemperatureReading
+
   case object TemperatureNotAvailable extends TemperatureReading
+
   case object DeviceNotAvailable extends TemperatureReading
+
   case object DeviceTimedOut extends TemperatureReading
+
 }
 
 class DeviceGroup(groupId: String) extends Actor with ActorLogging {
-  private var deviceIdToActor = Map.empty[String, ActorRef]  // TODO mutable state in actor
-  var actorToDeviceId = Map.empty[ActorRef, String]
+  import DeviceGroup._
 
   override def preStart(): Unit = log.info("DeviceGroup {} started", groupId)
 
   override def postStop(): Unit = log.info("DeviceGroup {} stopped", groupId)
 
   override def receive: Receive = {
+    receiveWithDeviceActors(Map.empty[String, ActorRef], Map.empty[ActorRef, String])
+  }
+
+  private def receiveWithDeviceActors(deviceIdToActor: Map[String, ActorRef], actorToDeviceId: Map[ActorRef, String]): Receive = {
     // only include the @ if need access to the top level object, in this case the RequestTrackDevice object
     case trackMsg@RequestTrackDevice(`groupId`, deviceId) =>
       deviceIdToActor.get(deviceId) match {
         case Some(deviceActor) =>
           deviceActor.forward(trackMsg)
         case None =>
-          val deviceActor = createDeviceActor(deviceId)
+          val deviceActor = context.actorOf(Device.props(groupId, deviceId), s"device-$deviceId")
+          context.watch(deviceActor)
+          val newDeviceIdToActor = deviceIdToActor + (deviceId -> deviceActor)
+          val newActorToDeviceId = actorToDeviceId + (deviceActor -> deviceId)
           deviceActor.forward(trackMsg)
+          context.become(receiveWithDeviceActors(newDeviceIdToActor, newActorToDeviceId))
       }
 
     case RequestTrackDevice(groupId, deviceId) =>
       log.warning(s"Rejecting TrackDevice request for groupId=$groupId. This actor is responsible for groupId=${this.groupId}")
       sender() ! DeviceManager.DeviceRegistrationRejected(groupId, deviceId, s"Rejecting TrackDevice request for groupId=$groupId. This actor is responsible for groupId=${this.groupId}")
-
-    case rejectedMsg@DeviceManager.DeviceRegistrationRejected(groupId, deviceId, originalMessage) =>
-      log.warning(s"TrackDevice request rejected unexpectedly from ${sender()} for $groupId-$deviceId: $originalMessage")
-      deviceIdToActor.get(deviceId) match {
-        case Some(deviceActor) if sender().eq(deviceActor) =>
-          log.error(s"Stop and remove corrupt device actor: $deviceActor")
-          context.stop(deviceActor)
-          deviceIdToActor -= deviceId
-//          val newDeviceActor = createDeviceActor(deviceId)
-//          newDeviceActor ! DeviceManager.RequestTrackDevice(groupId, deviceId) // TODO can get into an infinite loop
-        case None =>
-      }
 
     case RequestDeviceList(requestId) =>
       sender() ! ReplyDeviceList(requestId, deviceIdToActor.keySet)
@@ -62,20 +65,32 @@ class DeviceGroup(groupId: String) extends Actor with ActorLogging {
     case Terminated(deviceActor) =>
       val deviceId = actorToDeviceId(deviceActor)
       log.info("Device actor for {}-{} has been terminated. {}", this.groupId, deviceId, deviceActor)
-      actorToDeviceId -= deviceActor
-      deviceIdToActor -= deviceId
+      val newActorToDeviceId = actorToDeviceId - deviceActor
+      val newDeviceIdToActor = deviceIdToActor - deviceId
+      context.become(receiveWithDeviceActors(newDeviceIdToActor, newActorToDeviceId))
 
     case RequestAllTemperatures(requestId) =>
       log.info(s"RequestAllTemperatures $requestId")
+      var deviceRequestId: Long = 0
+      //      val requestIdToTemperature2: Map[Long, TemperatureReading] =
+      //        deviceIdToActor.map(
+      //          (deviceId: String, deviceActor: ActorRef) => {
+      //            deviceRequestId += 1
+      //            deviceActor ! Device.ReadTemperature(deviceRequestId)
+      //            (deviceRequestId -> DeviceNotAvailable)
+      //          }
+      //        )
 
-  }
+      val deviceActors: Seq[ActorRef] = deviceIdToActor.values.toSeq
+      val z: immutable.Map[Long, TemperatureReading] = immutable.Map()
 
-  private def createDeviceActor(deviceId: String): ActorRef = {
-    log.info("Creating device actor for device-{}", deviceId)
-    val deviceActor = context.actorOf(Device.props(groupId, deviceId), s"device-$deviceId")
-    deviceIdToActor += deviceId -> deviceActor
-    actorToDeviceId += deviceActor -> deviceId
-    context.watch(deviceActor)
-    deviceActor
+      val requestIdToTemperature: immutable.Map[Long, TemperatureReading] =
+        deviceActors.foldLeft(immutable.Map[Long, TemperatureReading]())(
+          (z : immutable.Map[Long, TemperatureReading], actor: ActorRef) => {
+            deviceRequestId += 1
+            actor ! Device.ReadTemperature(deviceRequestId)
+            z + (deviceRequestId -> DeviceNotAvailable)
+          }
+        )
   }
 }
