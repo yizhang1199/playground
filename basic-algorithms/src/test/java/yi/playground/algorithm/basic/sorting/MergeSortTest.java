b73b8da2a857293ddcc65d5d746088775c0033e1
Yi Zhang <yi_zhang@intuit.com>
2020-08-07 11:06:23 -0700

    updated

--- basic-algorithms/src/test/java/yi/playground/algorithm/basic/sorting/MergeSortTest.java
+++ basic-algorithms/src/test/java/yi/playground/algorithm/basic/sorting/MergeSortTest.java
@@ -1,12 +1,26 @@
 package yi.playground.algorithm.basic.sorting;
 
+import com.sun.tools.javac.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class MergeSortTest extends AbstractSortTest {
 
     @BeforeEach
     void setUp() {
         instance = new MergeSort();
+    }
+
+    @Test
+    void debug() {
+        List<String> list = List.of("Hello", "World");
+
+        list.forEach(element -> element.toLowerCase());
     }
 }
 
